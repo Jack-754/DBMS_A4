2a7f954ba33fc5cd271cdd7155be7d22ad30feb6
GhostDevanshu <dev_iitkgp2022@kgpian.iitkgp.ac.in>
2025-03-01 22:57:13 +0530

    added new base files

--- DBMS/__init__ref.py
+++ DBMS/__init__ref.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -11,20 +11,10 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 # Bcrypt instance for password hashing
 bcrypt = Bcrypt(app)
-
 # Login manager for user authentication
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-@login_manager.unauthorized_handler
-def unauthorized():
-    return jsonify(
-        {
-            "status": 100,
-            "error": "Unauthorized access", 
-            "message": "Please log in first"
-        }
-    )
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 
 from DBMS import routes
